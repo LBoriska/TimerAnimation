e8111af1da70f22f6a95fdba352a3b302818f163
tn <tn@GPBNB-21-08-015>
2023-08-15 12:34:40 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace TimerAnimation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.AmpelRot2 = new System.Windows.Forms.PictureBox();
+            this.AmpelRot = new System.Windows.Forms.PictureBox();
+            this.AmpelGruen2 = new System.Windows.Forms.PictureBox();
+            this.AmpelGruen = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AmpelRot2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AmpelRot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AmpelGruen2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AmpelGruen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.button1.Location = new System.Drawing.Point(504, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start / Stop";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 6000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.button2.Location = new System.Drawing.Point(504, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(245, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start / Stop";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::TimerAnimation.Properties.Resources.Masyanya;
+            this.pictureBox3.Location = new System.Drawing.Point(23, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(101, 116);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // AmpelRot2
+            // 
+            this.AmpelRot2.BackColor = System.Drawing.Color.Red;
+            this.AmpelRot2.Location = new System.Drawing.Point(131, 283);
+            this.AmpelRot2.Name = "AmpelRot2";
+            this.AmpelRot2.Size = new System.Drawing.Size(100, 30);
+            this.AmpelRot2.TabIndex = 3;
+            this.AmpelRot2.TabStop = false;
+            this.AmpelRot2.Visible = false;
+            // 
+            // AmpelRot
+            // 
+            this.AmpelRot.BackColor = System.Drawing.Color.Red;
+            this.AmpelRot.Location = new System.Drawing.Point(275, 161);
+            this.AmpelRot.Name = "AmpelRot";
+            this.AmpelRot.Size = new System.Drawing.Size(33, 152);
+            this.AmpelRot.TabIndex = 3;
+            this.AmpelRot.TabStop = false;
+            // 
+            // AmpelGruen2
+            // 
+            this.AmpelGruen2.BackColor = System.Drawing.Color.Lime;
+            this.AmpelGruen2.Location = new System.Drawing.Point(131, 283);
+            this.AmpelGruen2.Name = "AmpelGruen2";
+            this.AmpelGruen2.Size = new System.Drawing.Size(100, 30);
+            this.AmpelGruen2.TabIndex = 3;
+            this.AmpelGruen2.TabStop = false;
+            // 
+            // AmpelGruen
+            // 
+            this.AmpelGruen.BackColor = System.Drawing.Color.Lime;
+            this.AmpelGruen.Location = new System.Drawing.Point(275, 161);
+            this.AmpelGruen.Name = "AmpelGruen";
+            this.AmpelGruen.Size = new System.Drawing.Size(33, 152);
+            this.AmpelGruen.TabIndex = 3;
+            this.AmpelGruen.TabStop = false;
+            this.AmpelGruen.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TimerAnimation.Properties.Resources.wagen1;
+            this.pictureBox1.Location = new System.Drawing.Point(581, 333);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(178, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(549, 126);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(74, 35);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "auto1";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(74, 64);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "auto2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.AmpelRot2);
+            this.Controls.Add(this.AmpelRot);
+            this.Controls.Add(this.AmpelGruen2);
+            this.Controls.Add(this.AmpelGruen);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AmpelRot2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AmpelRot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AmpelGruen2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AmpelGruen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox AmpelGruen;
+        private System.Windows.Forms.PictureBox AmpelRot;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox AmpelRot2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox AmpelGruen2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerAnimation
+{
+    public partial class Form1 : Form
+    {
+        private int geschwHori = 5;
+        private int geschwVerti = 5;
+            public Form1()
+        {
+            InitializeComponent();
+            geschwHori = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {   //it was a timer
+            //label1.Text = (Convert.ToInt32(label1.Text) + 1).ToString(); 
+            //_______________________________________
+            //horizontal moving
+            
+            if (AmpelGruen.Visible == true
+            || pictureBox1.Location.X > AmpelGruen.Location.X + AmpelGruen.Width + 20
+            || pictureBox1.Location.X < AmpelGruen.Location.X)
+            { 
+                pictureBox1.Location = 
+                new Point(pictureBox1.Location.X - 30, pictureBox1.Location.Y);
+                
+                if (pictureBox1.Location.X <= - pictureBox1.Width)  // < = 0
+                {
+                pictureBox1.Location 
+                    = new Point(this.Width, pictureBox1.Location.Y);
+                 }
+            }
+
+            //__________________________________
+            //vertikal moving 
+
+            if (AmpelGruen2.Visible == true
+            || pictureBox3.Location.Y < AmpelGruen2.Location.Y - pictureBox3.Height 
+            || pictureBox3.Location.Y > AmpelGruen2.Location.Y + AmpelGruen2.Height
+            )
+            {
+                
+
+                pictureBox3.Location =
+                new Point(pictureBox3.Location.X, pictureBox3.Location.Y + 10);
+
+                if (pictureBox3.Location.Y <= -pictureBox3.Height)  
+                {
+                    pictureBox3.Location
+                        = new Point(this.Height, pictureBox3.Location.X);
+                }
+
+            }
+
+        }//void Timer1_Tick
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //timer1.Enabled = true; //Enabled is a boolean, false as a standart
+            timer1.Enabled = !timer1.Enabled; //on /off 
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {   
+            AmpelGruen.Visible = !AmpelGruen.Visible;   
+            AmpelRot.Visible = !AmpelRot.Visible;
+           
+            AmpelGruen2.Visible = !AmpelGruen2.Visible;
+            AmpelRot2.Visible = !AmpelRot2.Visible;
+        }
+    }
+}
